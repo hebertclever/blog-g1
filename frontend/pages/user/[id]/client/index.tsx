2781e05515978b4d473d80087ac069b4ec87b99a
Felipe M <fehmsilva92@gmail.com>
2023-09-28 22:17:50 -0300

    rota user

---
+++ frontend/pages/user/[id]/client/index.tsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+
+const Page = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    // Substitua isso pela sua função de busca de dados
+    fetch('http://127.0.0.1:8000/api/posts')
+      .then(response => response.json())
+      .then(data => setData(data));
+  }, []);
+
+  return (
+    <div>
+      <Link href="/">Página Inicial</Link>
+      <div>
+        <div className="flex justify-center font-bold text-blue-400 pb-3 pt-">
+          <h3 className="text-xs">NEWS & ARTICLE</h3>
+        </div>
+        <div className="flex justify-center pb-12">
+          <h1 className="flex text-4xl font-bold">Blog & Article</h1>
+        </div>
+        {data.map((item) => (
+          <div key={item.id} className="flex border-t-2">
+            <div>
+              <img src={item.image} alt={item.title} />
+            </div>
+            <div>
+              <p>{new Date(item.created_at).toLocaleDateString()}</p>
+              <p>{item.title}</p>
+              <Link href={`/posts/${item.id}`}>
+                <a>Leia Mais</a>
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Page;
--- frontend/src/app/user/[id]/page.js
+++
@@ -1,7 +0,0 @@
-const page = () => {
-  return <div>
-    <h1>user page</h1> <br/>
-    <a href="/"> Home page</a>
-  </div> ;
-};
-export default page;
