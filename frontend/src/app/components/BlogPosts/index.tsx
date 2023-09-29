18baf59f489c11a8292fd18e4c5d8d0c7f8b7008
hebertclever <hebertclever@gmail.com>
2023-09-29 14:54:29 -0300

    estilo do component Home de post em 90%

--- frontend/src/app/components/BlogPosts/index.tsx
+++ frontend/src/app/components/BlogPosts/index.tsx
@@ -31,49 +31,56 @@ export default function BlogPosts() {
     }
 
     return (
-        <div>
-            <h2 className='font-urbanist text-6xl font-bold mb-4'>Blog & Article</h2>
-            
-            <ul>
-                {posts.map(post => (
-                    <li key={post.id}>
-                        <h3>{post.title}</h3>
-                        <p>{post.content}</p>
-                        {post.image && (
-                            <Image 
-                                src={post.image} 
-                                alt={`Imagem de ${post.title}`} 
-                                width={500} 
-                                height={500} 
-                            />
-                        )}
-                    </li>
-                ))}
-            </ul>
+        <div className='flex justify-center items-center min-h-screen bg-gray-100'>
+            <div className='flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4 w-full max-w-5xl p-4'>
+                
+                <ul className='w-full sm:w-1/2'>
+                    {posts.slice(0, 1).map(post => (
+                        <li key={post.id}>
+                            {post.image && (
+                                <Image 
+                                    src={post.image} 
+                                    alt={`Imagem de ${post.title}`} 
+                                    width={500} 
+                                    height={500} 
+                                />
+                            )}
+                            <p className='text-gray-500 mb-2'>{post.created_at}</p>
+                            <h3 className='font-urbanist text-2xl font-bold leading-relaxed mb-2'>{post.title}</h3>
+                            <p>{post.content.substring(0, 100)}...</p> 
+                        </li>
+                    ))}
+                </ul>
     
-            <ul>
-                {posts.slice(0, 3).map((post, index) => (
-                    <li key={post.id} className='mb-6'>
-                        {post.image && (
-                            <img
-                                src={post.image}
-                                alt={post.title}
-                                className='mb-4'
-                                style={
-                                    index === 0
-                                        ? { width: '150px', height: '150px' }
-                                        : { width: '150px', height: '150px' }
-                                }
-                            />
-                        )}
-                        <p className='text-gray-500 mb-2'>{post.created_at}</p>
-                        <h3 className='font-urbanist text-2xl font-bold leading-relaxed mb-2'>{post.title}</h3>
-                        <p>{post.content}</p>
-                    </li>
-                ))}
-            </ul>
+                <ul className='w-full sm:w-1/2'>
+                    {posts.slice(0, 3).map((post, index) => (
+                        <li key={post.id} className='mb-6'>
+                            <div className='flex items-center mb-2'>
+                                {post.image && (
+                                    <img
+                                        src={post.image}
+                                        alt={post.title}
+                                        className='mr-2'
+                                        style={
+                                            { width: '50px', height: '50px' } 
+                                        }
+                                    />
+                                )}
+                                <div>
+                                    <p className='text-gray-500 mb-2'>{post.created_at}</p>
+                                    <h3 className='font-urbanist text-xl font-bold leading-relaxed mb-2'>{post.title}</h3>
+                                </div>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+                
+            </div>
         </div>
     );
+    
+    
+    
     
     
 
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,12 +4,7 @@ import BlogPosts from "./components/BlogPosts";
 export default function Home() {
   return (
     <div>
-      <BlogPosts/>
-      <h1 className="text-3xl font-bold underline">home page</h1>
-      <br />
-      <Link href="/user/1"> user page</Link>
-      <br />
-      <Link href="/1"> posts page</Link>
+      <BlogPosts/>    
     </div>
   );
 }
