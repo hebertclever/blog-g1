f3c733e4abcda3bed911ffd61a3af02b846d1e6d
carreivan <carreaguileraivan@gmail.com>
2023-10-02 10:56:05 -0300

    password input added

--- frontend/src/app/loginpage/page.jsx
+++ frontend/src/app/loginpage/page.jsx
@@ -5,15 +5,6 @@ const LoginPage = () => {
     <div className="w-screen h-screen flex flex-col justify-center items-center">
       <form className="w-[90%] h-[550px] md:w-[600px] md:p-10 md:border flex flex-col gap-5">
         <h2 className="self-start mb-5 font-semibold text-xl">Login</h2>
-        <div className="w-full flex flex-col gap-2">
-          <span>Name</span>
-          <input
-            className="border h-12 px-2"
-            type="text"
-            name="name"
-            placeholder="Enter your name"
-          />
-        </div>
 
         <div className="w-full flex flex-col gap-2">
           <span>Email</span>
@@ -22,6 +13,16 @@ const LoginPage = () => {
             type="email"
             name="email"
             placeholder="Enter your email"
+          />
+        </div>
+
+        <div className="w-full flex flex-col gap-2">
+          <span>Password</span>
+          <input
+            className="border h-12 px-2"
+            type="password"
+            name="password"
+            placeholder="Enter your password"
           />
         </div>
 
