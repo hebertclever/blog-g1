f55f1737e31a129bc8d95f3ff463b1100eb37d87
carreivan <carreaguileraivan@gmail.com>
2023-10-02 10:54:30 -0300

    register page

--- frontend/src/app/registerpage/page.jsx
+++ frontend/src/app/registerpage/page.jsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
 
-const LoginPage = () => {
+const RegisterPage = () => {
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center">
-      <form className="w-[90%] h-[550px] md:w-[600px] md:p-10 md:border flex flex-col gap-5">
-        <h2 className="self-start mb-5 font-semibold text-xl">Login</h2>
+      <form className="w-[90%] h-[650px] md:w-[600px] md:p-10 md:border flex flex-col gap-5">
+        <h2 className="self-start mb-5 font-semibold text-xl">Register</h2>
         <div className="w-full flex flex-col gap-2">
           <span>Name</span>
           <input
@@ -25,15 +25,25 @@ const LoginPage = () => {
           />
         </div>
 
+        <div className="w-full flex flex-col gap-2">
+          <span>Password</span>
+          <input
+            className="border h-12 px-2"
+            type="password"
+            name="password"
+            placeholder="Enter your password"
+          />
+        </div>
+
         <input
           type="submit"
-          value="Submit"
+          value="Register"
           className="border h-12 bg-blue-500 mb-12 text-white mt-10"
         />
         <span>
-          Do not you have an account yet?{" "}
-          <Link href="/registerpage" className="text-blue-500 underline">
-            Register
+          Do you already have an account?{" "}
+          <Link href="/loginpage" className="text-blue-500 underline">
+            Login
           </Link>
         </span>
       </form>
@@ -41,4 +51,4 @@ const LoginPage = () => {
   );
 };
 
-export default LoginPage;
+export default RegisterPage;
