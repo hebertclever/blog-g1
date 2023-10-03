43eb5e6ead4a9e474984464c42639710ba075331
Felipe M <fehmsilva92@gmail.com>
2023-10-02 22:19:24 -0300

    individual users

--- frontend/src/app/[id]/page.tsx
+++ frontend/src/app/[id]/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { useParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
 
@@ -25,25 +26,33 @@ const Post = () => {
       {post ? (
         <div className="w-screen h-auto flex flex-col px-5">
           <section className="w-full h-auto flex flex-col items-center mt-24 mb-32">
-            <span className="text-[#808D9E] text-[36px] font-semibold">
-              Jul 20, 2022
-            </span>
-            <p className="w-[350px] md:w-[1200px] font-semibold text-[#1D1E25] text-[40px] md:text-[72px] text-center mb-20">
-              <h1>{post.title}</h1>
-            </p>
-            <div className="w-full max-w-[1127px] h-[507px]  bg-[#E9ECF2] rounded-md">
-              <img src={post.image} alt={post.title} className="image" />
+            <div className="text-gray-400 text-4xl font-semibold">
+              {post.date
+                ? new Date(post.date).toLocaleDateString("en-US", {
+                    month: "short",
+                    day: "numeric",
+                    year: "numeric",
+                  })
+                : "Invalid date"}
+            </div>
+            <h1 className="w-[350px] md:w-[1200px] font-semibold text-[#1D1E25] text-[40px] md:text-[72px] text-center mb-20">
+              {post.title}
+            </h1>
+            <div className="w-full max-w-[1127px] h-[507px] bg-[#E9ECF2] rounded-md">
+              <img
+                src={post.image}
+                alt={post.title}
+                className="image w-full h-full object-cover"
+              />
             </div>
             <div className="max-w-[962px] h-auto flex flex-col">
               <h3 className="text-[#1D1E25] font-semibold text-[36px] mt-12">
-                The point of using Lorem Ipsum is that it has a more-or-less
-                normal distribution of letters, as opposed to using 'Content
-                here
+                {post.content ? post.content.substring(0, 120) : ""}
               </h3>
-              <p className="text-[#1D1E25] font-[500] text-[20px] mt-10">
+              <div className="text-[#1D1E25] font-[500] text-[20px] mt-10">
                 <p>{post.content}</p>
-              </p>
-            </div>{" "}
+              </div>
+            </div>
           </section>
         </div>
       ) : (
